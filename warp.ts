f2dba25062b36308529a1b6d0152606847e889d2
shuntofu <oya-taka.0629.0403@zeus.eonet.ne.jp>
2017-11-07 14:50:16 +0000

    Update warp.ts

---
+++ warp.ts
@@ -0,0 +1,11 @@
+const magik = magikcraft.io;
+
+function warp() {
+    const there = magik.memento.getItem('save');
+    if (there) {
+        magik.ianuae(there);
+    } else {
+        const here = magik.hic();
+        magik.memento(here);
+    }	
+}
