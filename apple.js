cb18f41221245f91765ff8e9553456a9a27d2738
shuntofu <oya-taka.0629.0403@zeus.eonet.ne.jp>
2017-11-08 10:11:15 +0000

    Update apple.js

--- apple.js
+++ apple.js
@@ -3,5 +3,9 @@ const magik = magikcraft.io;
 function apple() {
     for(var i=1;i<65;i++){
     	magik.satio();
+        magik.satio();
+        magik.satio();
+        magik.satio();
+        magik.satio();
     }
 }
