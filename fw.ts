3d966df0cb4dc0e7acbb9fd7141900a16dca87b4
shuntofu <oya-taka.0629.0403@zeus.eonet.ne.jp>
2017-11-27 09:04:52 +0000

    Update fw.ts

--- fw.ts
+++ fw.ts
@@ -3,5 +3,5 @@ const magik = magikcraft.io;
 function fw(times) {
      magik.doNTimes(function(){ 
          const there = magik.aspecto();
-     magik.stella(there);}, times, 1000);
+     magik.stella(there);}, times, 10);
 }
